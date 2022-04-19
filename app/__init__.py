fed823a9d9aa2b1cbed26342427f5ffffa3faa25
mugubr <miguimuniz@gmail.com>
2022-04-19 10:42:37 -0300

    Python-Flask App v2 - registration/login

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
 
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
 
-login = LoginManager(app)
-login.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 from flask_login import UserMixin
-from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +20,12 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -28,7 +34,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-        
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
